7e5095a02ad51c29c9d984ed1c4d90230f75bd09
Rino Montiel <rinofm@gmail.com>
2012-06-20 16:12:40 +0200

    perf improvements

--- bin/deploy/local2dev-aws.sh
+++ bin/deploy/local2dev-aws.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/www/fastclick
 
-rsync -crauivp --delete --exclude htdocs/bin --exclude htdocs/index.html --exclude example --exclude logs --exclude config.php --exclude .git\* . fastclick@dev-fastclick.crowdpark-cloud.com:www/fastclick
+rsync -crauivp --delete --exclude htdocs/bin --exclude Config.json --exclude htdocs/index.html --exclude example --exclude logs --exclude config.php --exclude .git\* . fastclick@dev-fastclick.crowdpark-cloud.com:www/fastclick

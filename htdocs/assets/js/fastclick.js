caeac3c4a26cf5f81dd3249b1888fe9897614606
Rino Montiel <rinofm@gmail.com>
2012-07-04 10:38:37 +0200

    debugging

--- htdocs/assets/js/fastclick.js
+++ htdocs/assets/js/fastclick.js
@@ -68,18 +68,19 @@ var crowdparkHTML = {
         var dataJson = JSON.stringify(data);
         dataObject = JSON.parse(dataJson);
 
-        FB.ui(
-            {
-                app_id:FACEBOOK_APP_ID,
-                method:'apprequests',
-                message:dataObject.message,
-                data:{
-                    'type':dataObject.type,
-                    'amount':dataObject.amount
-                },
-                title:'Send Gift',
-                to:dataObject.uid
+        var requestData = {
+            app_id:FACEBOOK_APP_ID,
+            method:'apprequests',
+            message:dataObject.message,
+            data:{
+                'type':dataObject.type,
+                'amount':dataObject.amount
             },
+            title:'Send Gift',
+            to:dataObject.uid
+        };
+
+        FB.ui(requestData,
 
             function (response) {
                 console.log("[RESPONSE Callback]");
